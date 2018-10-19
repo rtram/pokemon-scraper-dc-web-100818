09ed1db8254259b239b356e4c67dc610ff0dbb49
Robin Tram <robin.tram@gmail.com>
2018-10-19 19:23:10 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -26,7 +26,7 @@ class Pokemon
     
     row = db.execute(sql, id)[0]
         binding.pry
-    pikachu = Pokemon.new(row[0],row[1],row[2], db)
+    pikachu = Pokemon.new(id: row[0],name: row[1],type: row[2],db: db)
     pikachu
   end 
 
