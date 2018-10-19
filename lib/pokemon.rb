eaa81a38e827ceed40f63de4206a6d2922054693
Robin Tram <robin.tram@gmail.com>
2018-10-19 19:14:29 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,5 +16,15 @@ class Pokemon
     
     db.execute(sql, name, type)
   end
+  
+  def self.find(id, db)
+    sql = <<-SQL
+      SELECT *
+      FROM pokemon
+      WHERE id = ?
+    SQL
+    
+    db.execute(sql, id)
+  end 
 
 end
